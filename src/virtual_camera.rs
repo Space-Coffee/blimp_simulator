e9cc9c8f798b805a22dc30a49c21f3ee4e2bb9e4
Tomasz Kawalec <tkawalec369@gmail.com>
2025-09-11 17:23:09 +0200

    Downgrade Bevy and start it with appropriate plugins

--- src/virtual_camera.rs
+++ src/virtual_camera.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
+use bevy_headless_render;
 
 pub async fn virtual_camera_start() {
-    let bevy_app = App::new();
+    let mut bevy_app = App::new();
+    bevy_app
+        .add_plugins(DefaultPlugins)
+        .add_plugins(bevy_headless_render::HeadlessRenderPlugin);
+    bevy_app.run();
 }
