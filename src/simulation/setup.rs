a443ff15f32cabec4c137dafdc7c215bc5a45092
Asapros <cyanowakredka@gmail.com>
2025-10-20 18:36:55 +0200

    Add trees

--- src/simulation/setup.rs
+++ src/simulation/setup.rs
@@ -3,13 +3,31 @@ use bevy::color::Color;
 use bevy::gltf::GltfAssetLabel;
 use bevy::math::{Vec2, Vec3};
 use bevy::pbr::{DirectionalLight, MeshMaterial3d, StandardMaterial};
-use bevy::prelude::{
-    default, Camera, Commands, Mesh, Mesh3d, Plane3d, Query, Res, ResMut, SceneRoot, Transform,
-    With,
-};
+use bevy::prelude::*;
+use rand::Rng;
 use crate::render::camera::GroundCamera;
 use crate::simulation::physics::RigidBody;
 use crate::simulation::BlimpComponent;
+
+fn spawn_tree(cmds: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>, materials: &mut ResMut<Assets<StandardMaterial>>, root: Vec3, size: f32) {
+    let trunk_length = size / 2.0;
+    let trunk_thickness = size / 10.0;
+    let leaves_radius = size / 3.0;
+    // Trunk
+    cmds.spawn((
+        Mesh3d(meshes.add(Cuboid::new(trunk_thickness, trunk_length, trunk_thickness))),
+        MeshMaterial3d(materials.add(Color::srgb(0.55, 0.27, 0.07))),
+        Transform::from_translation(root + Vec3::new(0.0, trunk_length / 2.0, 0.0))
+    ));
+
+    // Leaves
+    cmds.spawn((
+        Mesh3d(meshes.add(Sphere::new(leaves_radius))),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.6, 0.0))),
+        Transform::from_translation(root + Vec3::new(0.0, trunk_length + leaves_radius, 0.0)),
+    ));
+
+}
 
 pub fn setup(
     mut cmds: Commands,
@@ -55,9 +73,9 @@ pub fn setup(
     // Floor
     let floor_mesh = meshes.add(Plane3d::new(
         Vec3::new(0.0, 1.0, 0.0),
-        Vec2::new(100.0, 100.0),
+        Vec2::new(500.0, 500.0),
     ));
-    let floor_material = materials.add(asset_server.load("debug.png"));
+    let floor_material = materials.add(asset_server.load("grass.png"));
     cmds.spawn((
         Mesh3d(floor_mesh),
         MeshMaterial3d(floor_material),
@@ -68,4 +86,14 @@ pub fn setup(
     let mut transform = camera_transform.get_single_mut().expect("Camera not found");
     *transform = Transform::from_xyz(16.0, 32.0, 64.0)
         .looking_at(Vec3::new(0.0, 8.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
+    let mut rng = rand::rng();
+    
+    for _ in 0..400 {
+        let x = rng.random_range(-500.0f32..500.0f32);
+        let z = rng.random_range(-500.0f32..500.0f32);
+        let size = rng.random_range(5.0f32..25.0f32);
+
+        spawn_tree(&mut cmds, &mut meshes, &mut materials, Vec3::new(x, 0.0, z), size);
+    }
+
 }
